fd5d3c19a99b72f6c7553d71a98c19b20f81a630
Lucas Izidoro <luckoito@icloud.com>
2023-01-20 19:06:03 -0300

    Página de grade e scripts atualizados

--- Scripts/home.js
+++ Scripts/home.js
@@ -2,17 +2,19 @@ const urlL = "https://localhost:7214/api/Materias"
 const urlT = "https://randomuser.me/api/?results=10"
 const userDetails = [...document.querySelectorAll("#profileName,#profileImage,#profileRole")]
 
-/*
+const materiaDetails = [...document.querySelectorAll(".materiaNome id0, .materiaSemestre id0, .materiaTurno id0, .materiaCurso id0, .materiaDia id0, .materiaSala id0, .materiaProfessor id0")]
+
+
 fetch(urlL)
-.then ((resp))
+.then ((resp) => resp.json())
 .then (function(data){
-    console.log(data.results)
+    console.log(data)
 })
 .catch (function(error){
     console.log(error)
 })
-*/
 
+/*
 fetch (urlT)
 .then ((resp) => resp.json())
 .then (function(data){
@@ -35,3 +37,4 @@ fetch (urlT)
 .catch(function(error){
     console.log(error)
 }) 
+*/
